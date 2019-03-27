4d6021888201326b285062657942fd3af8cbc195
Wouter Raateland <wouterraateland@gmail.com>
2019-03-27 22:01:13 +0100

    Update texts

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
+    <h1>Public Workspaces.</h1>
+    <p>Enjoy working, enjoy your coffee.</p>
     <p>Now go build something great.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
