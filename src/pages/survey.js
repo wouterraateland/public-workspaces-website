757ea1fae4bd230e6416418258ee11534890cb36
Wouter Raateland <wouterraateland@gmail.com>
2019-05-08 09:44:17 +0200

    Create survey redirect page

---
+++ src/pages/survey.js
@@ -0,0 +1,8 @@
+const SurveyPage = () => {
+  if (typeof window !== "undefined") {
+    window.location.replace("https://forms.gle/GAPCuZqt5Riwwe2Z9");
+  }
+  return null;
+};
+
+export default SurveyPage;
